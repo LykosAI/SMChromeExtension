ca80501c935a36d87cd66872e5939c0a984921ad
JT <mohnjiles@gmail.com>
2024-03-13 19:28:41 -0700

    Initial commit, working in chrome

---
+++ README.md
@@ -0,0 +1,2 @@
+## Stability Matrix Chrome Extension
+Adds a "Download with Stability Matrix" button to the Files section of CivitAI models. Only tested on Chrome so far.
---
+++ script.js
@@ -0,0 +1,70 @@
+// Content script
+function addButton() {
+  // Select all the existing download buttons
+  const downloadButtons = document.querySelectorAll(
+    '.mantine-UnstyledButton-root[href^="/api/download/models/"]'
+  );
+  const createButton = document.querySelectorAll(
+    '.mantine-UnstyledButton-root[data-activity="create:model"]'
+  );
+
+  // Loop through each download button
+  downloadButtons.forEach((downloadButton) => {
+    // Get the parent container of the download button
+    const buttonContainer = downloadButton.parentNode;
+    const downloadParams = downloadButton.href.split('?')[1];
+
+    // Check if the create button exists
+    if (downloadParams) {
+      // Create a new button element
+      const newButton = document.createElement("a");
+
+      newButton.className =
+        "mantine-UnstyledButton-root mantine-Button-root mantine-4fe1an";
+      newButton.style.backgroundColor = "#68339f";
+      newButton.style.border = "none";
+      newButton.style.borderRadius = "4px";
+      newButton.style.color = "#fff";
+      newButton.style.cursor = "pointer";
+
+      // Get the IDs from the URL
+      const url = new URL(window.location.href);
+      let modelId = url.pathname.split("/").pop();
+      // Check if the modelId is an integer, and if not, pop again
+      if (isNaN(parseInt(modelId))) {
+        modelId = url.pathname.split("/")[2];
+      }
+      const modelVersionId = url.searchParams.get("modelVersionId");
+
+      // Create the link based on the IDs
+      const link = `stabilitymatrix://downloadCivitModel?modelid=${modelId}${
+        modelVersionId ? `&modelVersionId=${modelVersionId}&${downloadParams}` : `&${downloadParams}`
+      }`;
+
+      newButton.href = link;
+
+      // Create the inner content of the button
+      const buttonInner = document.createElement("div");
+      buttonInner.className = "mantine-3xbgk5 mantine-Button-inner";
+
+      const buttonLabel = document.createElement("span");
+      buttonLabel.className = "mantine-qo1k2 mantine-Button-label";
+      buttonLabel.textContent = "Download with Stability Matrix";
+
+      buttonInner.appendChild(buttonLabel);
+      newButton.appendChild(buttonInner);
+
+      // Insert the new button after the button container
+      buttonContainer.parentNode.insertBefore(
+        newButton,
+        buttonContainer.previousSibling
+      );
+    }
+  });
+}
+addButton();
+// Inject the content script into the page
+const buttons = document.querySelectorAll('div[data-radix-scroll-area-viewport] .mantine-UnstyledButton-root[data-button="true"]');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => setTimeout(addButton, 500));
+});
